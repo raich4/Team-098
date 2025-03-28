78efb358306066dabcb85bddaf70cca34be456b9
jmsMacbook <jmswws75@gmail.com>
2025-03-27 22:46:43 -0400

    Test push. Sample code for item.

---
+++ Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    String name;
+    String type;
+    int[] expiryDate;
+    boolean isDefective;
+
+    public Item(String name, String type, int expYr, int expMon, int expDay, boolean isDefective) {
+
+    }
+}
+
+
