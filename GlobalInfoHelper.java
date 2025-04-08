ba5e23a1435de3d2cc5ef50b360e1541713fadbc
James <jmswws75@gmail.com>
2025-04-07 23:59:13 -0400

    Global info modifications

--- GlobalInfoHelper.java
+++ GlobalInfoHelper.java
@@ -2,45 +2,45 @@ public class GlobalInfoHelper {
 
     // A static array that can be accessed anywhere
     public static String[] Categories = {
-            "Pet food and accessories" ,
-            "Baking" ,
-            "Oil" ,
-            "Instant meals" ,
-            "Side dishes" ,
-            "Condiments" ,
-            "Packaged milk" ,
-            "Hot beverages",
-            "Coffee",
-            "High demand (gluten-free, brown rice, energy drinks, instant coffee)",
-            "Medicine",
-            "Canned lentils",
-            "Dry lentils",
-            "Dry chickpeas",
-            "Peanut butter and Nutella",
-            "Kraft Dinner",
-            "Crackers",
-            "Granola bars",
-            "Cookies",
-            "Miscellaneous snacks",
-            "Chips",
-            "Fish and meat",
-            "Soup/stew with meat",
-            "Soup/stew vegetarian",
-            "Packaged fruit",
-            "Pasta sauce/canned tomatoes",
-            "Pasta and Noodles",
-            "Rice",
-            "Packaged vegetables",
-            "Canned beans",
-            "Dry beans",
-            "Baked beans",
-            "Baby food and formula",
-            "Bottled water",
-            "Flour",
-            "Juice",
-            "Cold beverages",
-            "Oatmeal",
-            "Cereal"
+            "Pet food and accessories" , //5x
+            "Baking" , // 1x
+            "Oil" , //5x
+            "Instant meals" , //2x
+            "Side dishes" , //2x
+            "Condiments" , //1x
+            "Packaged milk" , //1x
+            "Hot beverages", //0.5x
+            "Coffee", //2x
+            "High demand (gluten-free, brown rice, energy drinks, instant coffee)", //2x
+            "Medicine", //0.5x
+            "Canned lentils", //1x
+            "Dry lentils", //1x
+            "Dry chickpeas", //1x
+            "Peanut butter and Nutella", //1x
+            "Kraft Dinner", //2x
+            "Crackers", //2x
+            "Granola bars", //1x
+            "Cookies", //1x
+            "Miscellaneous snacks", //2x
+            "Chips", //4x
+            "Fish and meat", //0.5x
+            "Soup/stew with meat", //1x
+            "Soup/stew vegetarian", //1x
+            "Packaged fruit", //1x
+            "Pasta sauce/canned tomatoes", //1x
+            "Pasta and Noodles", //5x
+            "Rice", //10x
+            "Packaged vegetables", //2x
+            "Canned beans", //1x
+            "Dry beans", //1x
+            "Baked beans", //1x
+            "Baby food and formula", // ?
+            "Bottled water", //10x
+            "Flour", //10x
+            "Juice", //1x
+            "Cold beverages", //1x
+            "Oatmeal", //5x
+            "Cereal" //5x
     };
 
 }
